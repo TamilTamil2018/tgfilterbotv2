17ad0eb79066b3f16b8de2ce950ac23746f32619
TamilTamil2018 <104403030+TamilTamil2018@users.noreply.github.com>
2022-06-12 09:59:00 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/Team_idiots"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/Team_idiots'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/TamilTamil2018/tgfilterbotv2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/Team_idiots')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
